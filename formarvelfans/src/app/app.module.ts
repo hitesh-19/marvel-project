7640b5069ea62df99873ed4450dfb6d1487c089f
Hitesh Ahirwar <hiteshahirwar1902@gmail.com>
2019-10-31 18:17:32 +0530

    bootstrap 4 added

--- formarvelfans/src/app/app.module.ts
+++ formarvelfans/src/app/app.module.ts
@@ -2,6 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { CharactersModule } from './characters/characters.module';
+import { ComicsModule } from './comics/comics.module';
+import { FanclubModule } from './fanclub/fanclub.module';
+import { LandingModule } from './landing/landing.module';
+import { MoviesModule } from './movies/movies.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -14,7 +19,12 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CharactersModule,
+    ComicsModule,
+    FanclubModule,
+    LandingModule,
+    MoviesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
