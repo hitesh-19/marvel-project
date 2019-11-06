98995a2c15e12d4f9a92bec98f6ae3cd37758aed
Hitesh Ahirwar <hiteshahirwar1902@gmail.com>
2019-11-07 00:07:27 +0530

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/formarvelfan'));
+app.use(express.static('./dist/formarvelfans'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/formarvelfan/index.html'));
+res.sendFile(path.join(__dirname,'/dist/formarvelfans/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
