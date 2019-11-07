648dcfcac5ae4dbb873b7fe3156b513f3d28522d
Hitesh Ahirwar <hiteshahirwar1902@gmail.com>
2019-11-07 15:04:11 +0530

    landing pages changes

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
+  public kevinFeige: string;
+  public stanLee: string;
+  public i: number = 0;
+
   constructor() { }
 
   ngOnInit() {
+
+    setInterval(() => {
+      if(this.i === 0) {
+        this.kevinFeige = '../../assets/images/kevinfeige.jpg'
+        this.stanLee = '../../assets/images/stanlee1.jpg'
+      } else if(this.i === 1) {
+        this.kevinFeige = '../../assets/images/kevinfeige2.jpg'
+        this.stanLee = '../../assets/images/stanlee2.jpg'
+      } else if(this.i === 2){
+        this.kevinFeige = '../../assets/images/kevinfeige3.jpg'
+        this.stanLee = '../../assets/images/stanlee3.jpg'
+      }
+      this.i++;
+      if(this.i === 3) {
+        this.i = 0;
+      }
+    }, 2000)
   }
 
 }
